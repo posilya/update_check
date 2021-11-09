45bf2ab20ab37b0b3b522beef3712d1805152e9a
posilya <pos_ilya@vk.com>
2021-11-09 11:54:56 +0500

    Запись в файл

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import requests
+# coding=utf-8
+
 from urllib.parse import urljoin
 from lxml import html
+import requests
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 site = 'https://www.uksap.ru/'
 array_links = '//div[@class="news-list"][1]' + '//a'
@@ -11,5 +16,8 @@ if response.status_code == 200:
 
     links = [urljoin(site, link.attrib['href']) for link in tree.xpath(array_links)]
     print(links)
+
+    with open(os.path.join(BASE_DIR, 'links'), 'w') as file:
+        file.write('\n'.join(links))
 else:
     print('Сайт вернул', response.status_code)
