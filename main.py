7ea600ac16e701aa9a7c7f064640f113a33971cf
posilya <pos_ilya@vk.com>
2021-11-09 12:25:00 +0500

    Поиск новых ссылок

--- main.py
+++ main.py
@@ -5,6 +5,17 @@ from lxml import html
 import requests
 import os
 
+
+def new_links(old, actual):
+    r = []
+    for i in actual:
+        if i not in old:
+            r.append(i)
+            old.append(i)  # избавляемся от дубликатов
+            print(i)
+    return r
+
+
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 site = 'https://www.uksap.ru/'
@@ -22,7 +33,9 @@ if response.status_code == 200:
     tree = html.fromstring(response.content)
 
     links = [urljoin(site, link.attrib['href']) for link in tree.xpath(array_links)]
-    print(links)
+    links = list(set(links))
+
+    new = new_links(old_links, links)
 
     with open(os.path.join(BASE_DIR, 'links'), 'w') as file:
         file.write('\n'.join(links))
